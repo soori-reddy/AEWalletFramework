7141144923bab89605fa64e4ee815a0eafb31cdf
akshgaur <aksh.gaur@alertenterprise.com>
2024-05-28 18:08:02 +0530

    First commit

--- Sources/AEWalletFramework/AEWalletFramework.swift
+++ Sources/AEWalletFramework/AEWalletFramework.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+public struct AEWalletFramework{
+    public static func doSomeWork(){
+        print("Doing some work..")
+    }
+}
