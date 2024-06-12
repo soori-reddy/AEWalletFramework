51445a2bbe47121d83fe9e3a9133ada1d133a71a
akshgaur <aksh.gaur@alertenterprise.com>
2024-06-12 17:09:28 +0530

    update

--- Sources/AEWalletFramework/AEWalletFramework.swift
+++ Sources/AEWalletFramework/AEWalletFramework.swift
@@ -12,7 +12,7 @@ public struct AEWalletFramework{
     public init(context:ProvisioningContext?, accessToken:String, accessTokenExpiration:Double) {
         let settingsManager = SettingsManager.shared()
         settingsManager.setAccessToken(authToken: accessToken)
-        settingsManager.setServerURL(serverURL: "partner/v1/prepareProvisioning")
+        settingsManager.setServerURL(serverURL: "https://nfcqalocal.alertenterprise.com")
 //        settingsManager.setServerPort(serverPort: String(serverConfig!.port))
         settingsManager.setAccessTokenExpiration(accessTokenExpiration: accessTokenExpiration)
         let context = ProvisioningContext(product: "hospitality", credentialType: "hospitality" + "-credential-type", cardTemplateIdentifier: "1234", passDefinitionIdentifier: nil)
